935bff772b48e5d36c32a97daa397dd03a001bfd
qypec <qypec.prog@yandex.ru>
2019-08-05 19:56:35 +0300

    deleted some unnecessary functions

--- ft_matr/ft_matr.h
+++ ft_matr/ft_matr.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_matr.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qypec <qypec@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 15:32:02 by qypec             #+#    #+#             */
-/*   Updated: 2019/08/03 18:23:39 by qypec            ###   ########.fr       */
+/*   Updated: 2019/08/05 19:35:58 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ char					**ft_matrnew(size_t matr_size, ...);
 char					**ft_matrmemalloc(size_t size);
 char					**ft_matrdup(const char *first_str, ...);
 void					*ft_matrdel(char ***ret);
+size_t					ft_matrlen(const char **matr);
 
 #endif
