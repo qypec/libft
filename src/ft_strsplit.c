a090e2a92cfe1c9b1dd5ea60ccdb07ce180e6b48
qypec <qypec.prog@yandex.ru>
2019-12-19 12:38:54 +0300

    fixed ft_strsplit

--- src/ft_strsplit.c
+++ src/ft_strsplit.c
@@ -12,18 +12,26 @@
 
 #include "libft.h"
 
-size_t				get_split_len(const char *s, int c)
+size_t				get_split_len(char **str, int c)
 {
 	size_t			len;
-
+	char			*s;
+	
+	while (**str == c && **str != '\0')
+		(*str)++;
+	s = *str;
 	len = 0;
 	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			len++;
-		s++;
+			while (*s == c && *s != '\0')
+				s++;
+		}
+		s += ((*s == '\0') ? 0 : 1);
 	}
-	return (len + 1);
+	return (((len == 0) ? 0 : len));
 }
 
 char				**ft_strsplit(char const *s, char c)
@@ -34,7 +42,7 @@ char				**ft_strsplit(char const *s, char c)
 	size_t			i;
 
 	if ((matr = (char **)ft_matrmemalloc(sizeof(char *) * \
-								(get_split_len(s, c) + 1))) == NULL)
+			(get_split_len((char **)&s, c)))) == NULL)
 		return (NULL);
 	i = 0;
 	while (*s != '\0')
@@ -47,6 +55,8 @@ char				**ft_strsplit(char const *s, char c)
 			return (ft_matrdel(&matr));
 		ft_memcpy(matr[i++], s, len);
 		(s[len] == '\0') ? (s += len) : (s += len + 1);
+		while (*s != '\0' && *s == c)
+			s++;
 	}
 	return (matr);
 }
