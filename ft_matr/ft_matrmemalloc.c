9776d153ef9cb4497a4682a5faa345ef41e83c2e
qypec <qypec.prog@yandex.ru>
2020-01-03 15:11:25 +0300

    fixed ft_matrmemalloc.c

--- ft_matr/ft_matrmemalloc.c
+++ ft_matr/ft_matrmemalloc.c
@@ -6,7 +6,7 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 15:50:35 by qypec             #+#    #+#             */
-/*   Updated: 2019/11/19 16:14:16 by yquaro           ###   ########.fr       */
+/*   Updated: 2020/01/03 14:09:42 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ char				**ft_matrmemalloc(size_t size)
 	char			**matr;
 	int				i;
 
+	if (size == 0)
+		return (NULL);
 	if ((matr = (char **)malloc(sizeof(char *) * size)) == NULL)
 		return (NULL);
 	i = 0;
