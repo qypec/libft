609fda20cbb8ad252f99a0198172e50e1c0a6bef
qypec <qypec.prog@yandex.ru>
2020-01-20 12:40:16 +0300

    rewrited ft_ismapitem.c

--- ft_map/ft_ismapitem.c
+++ ft_map/ft_ismapitem.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ismapitem.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qypec <qypec@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 10:41:43 by yquaro            #+#    #+#             */
-/*   Updated: 2019/12/11 23:49:01 by qypec            ###   ########.fr       */
+/*   Updated: 2020/01/20 12:19:44 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,16 @@
 
 int					ft_ismapitem(t_map *map, const char *key)
 {
-	if (ft_mapvalue(map, key) == NULL)
-		return (0);
-	return (1);
+	t_list			*maplst;
+	int				(*hashfunc)(const char *, int);
+
+	hashfunc = map->hashfunc;
+	maplst = map->array[hashfunc(key, map->size)];
+	while (maplst != NULL)
+	{
+		if (ft_strequ(((t_htab *)(maplst->content))->key, key))
+			return (1);
+		maplst = maplst->next;
+	}
+	return (0);
 }
